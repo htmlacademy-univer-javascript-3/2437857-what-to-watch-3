68b2c93b4950076e76d244d31e917bf4d8297c17
TashK101 <Natalia.Karimova@urfu.me>
2023-11-10 17:02:11 +0500

    маршрут по тайным тропам

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import MainPage from '../pages/main-page/main-page';
+import NotFound from '../pages/four-oh-four-page/four-oh-four-page';
+import AddReviewPage from '../pages/add-review-page/add-review-page';
+import FilmPage from '../pages/film-page/film-page';
+import MyListPage from '../pages/my-list-page/my-list-page';
+import PlayerPage from '../pages/player/player-page';
+import SignInPage from '../pages/sign-in/sign-in-page';
+import AuthorisedContent from '../AuthorisedContent/AuthorisedContent';
+
 
 type AppProps = {
   title: string;
@@ -8,7 +18,22 @@ type AppProps = {
 
 function App(props: AppProps): JSX.Element {
   return (
-    <MainPage {...props} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage {...props}/>} />
+        <Route path="/login" element={<SignInPage />} />
+        <Route path="/mylist" element={
+          <AuthorisedContent authorizationStatus>
+            <MyListPage />
+          </AuthorisedContent>
+        }
+        />
+        <Route path="/films/:id" element={<FilmPage />} />
+        <Route path="/films/:id/review" element={<AddReviewPage />} />
+        <Route path="/player/:id" element={<PlayerPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
