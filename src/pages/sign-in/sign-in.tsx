72f18b6668f01848976166feff2d710356f51d9b
TashK101 <Natalia.Karimova@urfu.me>
2023-12-15 03:03:17 +0500

    Обратная сторона реальности

--- src/pages/sign-in/sign-in.tsx
+++ src/pages/sign-in/sign-in.tsx
@@ -1,4 +1,3 @@
-
 export function SignIn() {
   return (
     <div className="user-page">
@@ -19,13 +18,13 @@ export function SignIn() {
           <div className="sign-in__fields">
             <div className="sign-in__field">
               <input className="sign-in__input" type="email" placeholder="Email address" name="user-email"
-                     id="user-email"
+                id="user-email"
               />
               <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
             <div className="sign-in__field">
               <input className="sign-in__input" type="password" placeholder="Password" name="user-password"
-                     id="user-password"
+                id="user-password"
               />
               <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
